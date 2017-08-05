46e279cc04202e6a1f839d5d011fcfd74c5337b4
IAM DEV <elky@iamdeveloper.io>
2017-08-05 19:26:08 +0200

    Change made on ERC20.sol OpenZeppelin Code. Make it more standard to ERC20 token

--- installed_contracts/zeppelin/contracts/token/ERC20.sol
+++ installed_contracts/zeppelin/contracts/token/ERC20.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.13;
 
 
 import './ERC20Basic.sol';
@@ -7,10 +7,17 @@ import './ERC20Basic.sol';
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
+ *
+ * created by OpenZeppelin 
+ * https://github.com/OpenZeppelin/zeppelin-solidity
+ * changed by Elky Bachtiar (IaM <DEV>)
+ * https://iamdeveloper.io
+ * Changes made base on ERC20 Token Stadard and Solidity version 0.4.13
+ * https://theethereum.wiki/w/index.php/ERC20_Token_Standard
  */
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+  function approve(address _spender, uint256 _value) returns (bool success);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
