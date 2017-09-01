a41a3352cd6b3ac53f8a21fcabdd2d0aebe19a55
IAM DEV <elky@iamdeveloper.io>
2017-09-01 12:47:13 +0200

    interface controller added

---
+++ contracts/interface/ApproveAndCallReceiver.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.15;
+
+/*
+  Copyright 2017, Jordi Baylina (Giveth)
+  
+ * Changes made by IAM <DEV> (Elky Bachtiar) 
+ * https://www.iamdeveloper.io
+*/
+
+contract ApproveAndCallReceiver {
+    function receiveApproval(address _from, uint256 _amount, address _token, bytes _data);
+}
