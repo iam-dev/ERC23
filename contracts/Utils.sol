92bb9c820b2042fda09be6542d4f1cb05e7edf78
IAM DEV <elky@iamdeveloper.io>
2017-09-14 05:57:55 +0200

    Add greaterThanNow to Utils.sol

--- contracts/Utils.sol
+++ contracts/Utils.sol
@@ -22,14 +22,14 @@ contract Utils {
 
     // validates an address - currently only checks that it isn't null
     modifier validAddress(address _address) {
-        require(_address != 0x0 || _address != address(0) || _address != 0);
+        require(_address != 0x0 && _address != address(0) && _address != 0);
         _;
     }
 
     // validates multiple addresses - currently only checks that it isn't null
     modifier validAddresses(address _address, address _anotherAddress) {
-        require((_address != 0x0         || _address != address(0)        || _address != 0 ) &&
-                ( _anotherAddress != 0x0 || _anotherAddress != address(0) || _anotherAddress != 0)
+        require((_address != 0x0         && _address != address(0)        && _address != 0 ) &&
+                ( _anotherAddress != 0x0 && _anotherAddress != address(0) && _anotherAddress != 0)
         );
         _;
     }
@@ -52,5 +52,11 @@ contract Utils {
         bytes memory tempInput = bytes(_input);
         require(tempInput.length != 0);
         _; 
+    }
+
+    // verifies that an amount is greater than zero
+    modifier greaterThanNow(uint256 _startTime) {
+         require(_startTime >= now);
+        _;
     }
 }
